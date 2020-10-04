52eb4d72c61a3a849fae1defea3b0c812fdcd752
陳柏蓉 <jerry19920702@gmail.com>
2020-10-04 16:43:50 +0800

    first finished

--- script.js
+++ script.js
@@ -5,10 +5,56 @@ $(document).ready(function () {
     } else {
       $(".navbar").removeClass("sticky")
     }
+
+
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show")
+    } else {
+      $(".scroll-up-btn").removeClass("show")
+    }
+  })
+
+  $('.scroll-up-btn').click(function () {
+    $('html').animate({ scrollTop: 0 })
+  })
+
+  new Typed(".typing", {
+    strings: ["Developer", "Blogger", "Learner"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  })
+  new Typed(".typing-2", {
+    strings: ["Blogger", "Developer", "FrontEnd"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
   })
 
   $(".menu-btn").click(function () {
     $(".menu").toggleClass("active")
     $(".menu-btn i").toggleClass("active")
   })
+
+  $(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplay: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false
+      },
+      600: {
+        items: 2,
+        nav: false
+      },
+      1000: {
+        items: 3,
+        nav: false
+      }
+    }
+  });
 })
